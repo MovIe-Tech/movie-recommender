89ad119ce6afd17286cb727ac2f1568ace1f0043
ハセガワタイ <hasegawatai@hasegawatainoMacBook-Air.local>
2019-12-20 15:03:01 +0900

    doc2vec修正

--- predict/doc2vec.py
+++ predict/doc2vec.py
@@ -5,8 +5,14 @@ import sys
 #!pip install googletrans
 from gensim.models.doc2vec import Doc2Vec
 from gensim.models.doc2vec import TaggedDocument
+from utils.preprocess import preprocess_TextToList
 
-
+# ストップワードを用い、テキストを単語のリストにする
+def preprocess_TextToList(text, stopwords_path='../data/stopwords.csv'):
+    splitted_reviews = analysis(text).split(' ')
+    stopwords = pd.read_csv(stopwords_path, encoding='utf-8').T.values.tolist()[0]
+    return [word for word in splitted_reviews if word not in stopwords]
+    
 # Doc2Vecのモデルを保存
 def save_model(words_list, titles_list, model_path):
     trainings = [TaggedDocument(words = words ,tags = [titles_list[i]]) for i, words in enumerate(words_list)]
