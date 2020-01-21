cb88395a4755992c38120459baeb23396da003ab
ハセガワタイ <hasegawatai@hasegawatainoMacBook-Air.local>
2020-01-21 12:01:20 +0900

    v1 run.py

--- .ipynb_checkpoints/run-checkpoint.py
+++ .ipynb_checkpoints/run-checkpoint.py
@@ -6,5 +6,11 @@ from prediction import doc2vec
 from utils.preprocess import preprocess_TextToList
 
 
-def search_for_movies(query):
+def search_for_movies(query, topn=10, w_r_d=1, w_r_f=1, w_r_l=1, w_r_t=1):
+    title_list = pd.read_csv("data/movies_data.csv")['title'].values.tolist()
+    rate_list = pd.read_csv("data/movies_data.csv")['rate'].values.tolist()
     query_list = preprocess_TextToList(query)
+    review_pred = doc2vec.predict_movies(input_list = query_list, topn=1005, model_path='data/doc2vec_reviews.model')
+    pred_list = review_pred
+    sorted_id = np.argsort(pred_list)[::-1]
+    return np.array(title_list)[np.array(sorted_id)][:topn], np.array(rate_list)[np.array(sorted_id)][:topn]
--- .ipynb_checkpoints/temp-checkpoint.py
+++ .ipynb_checkpoints/temp-checkpoint.py
@@ -3,13 +3,17 @@ import sys
 from utils.preprocess import preprocess_TextToList
 from prediction import lsi
 from prediction import doc2vec
+from run import search_for_movies
 
 
-args = sys.argv
-input_text = args[1]
-input_list = preprocess_TextToList(input_text)
-sim = lsi.predict_movies(input_list)
-sim_sorted = np.sort(sim)[::-1]
-index_sorted = np.argsort(sim)[::-1]
-print(sim_sorted[:5])
-print(index_sorted[:5])
+# args = sys.argv
+# input_text = args[1]
+# input_list = preprocess_TextToList(input_text)
+# sim = lsi.predict_movies(input_list)
+# sim_sorted = np.sort(sim)[::-1]
+# index_sorted = np.argsort(sim)[::-1]
+# print(sim_sorted[:5])
+# print(index_sorted[:5])
+title_list, rate_list = search_for_movies(query="悲しい気持ち")
+print(title_list)
+print(rate_list)
