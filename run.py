f9c64ee515a4279676d2110d5f6ac556a54597cd
ハセガワタイ <hasegawatai@hasegawatainoMacBook-Air.local>
2020-01-21 11:20:55 +0900

    add movies data

--- run.py
+++ run.py
@@ -6,5 +6,6 @@ from prediction import doc2vec
 from utils.preprocess import preprocess_TextToList
 
 
-def search_for_movies(query):
+def search_for_movies(query, topn, w_r_d=1, w_r_f=1, w_r_l=1, w_r_t=1):
     query_list = preprocess_TextToList(query)
+    review_pred = doc2vec.predict_movies(input_list = query_list, topn=1005, model_path='data/doc2vec_review.model')
