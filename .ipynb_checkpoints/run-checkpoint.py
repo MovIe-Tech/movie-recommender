97fb0eed25163a9a8f520f74f1b13a0a40e98c40
ハセガワタイ <hasegawatai@hasegawatainoMacBook-Air.local>
2020-01-25 14:31:40 +0900

    implement doc2vec

--- .ipynb_checkpoints/run-checkpoint.py
+++ .ipynb_checkpoints/run-checkpoint.py
@@ -9,33 +9,33 @@ from prediction import query_expansion
 from prediction import TFIDF
 
 
-def search_for_movies(query, topn=10, w_review = 1, w_syn = 1, w_r_d=1, w_r_f=1, w_r_l=1, w_r_t=1):
+def search_for_movies(query, topn=10, w_review=1, w_syn=1, w_rl=1, w_sl=1, w_rd=1, w_sd=1, w_rf=1, w_sf=1, w_rt=1, w_st=1):
     title_list = pd.read_csv("data/movies.csv")['title'].values.tolist()
     rate_list = pd.read_csv("data/movies.csv")['rate'].values.tolist()
     
     # remove here after inplement query expansion
     query_list_review = preprocess_TextToList(query, stopwords_path='data/stop_words_review.csv')
-    quety_list_syn = preprocess_TextToList(query, stopwords_path='data/stop_words_synopsis.csv')
+    query_list_syn = preprocess_TextToList(query, stopwords_path='data/stop_words_synopsis.csv')
     
     # query expansion
     #query_list_review = query_expansion.expansion_magic(query, 8, stopwords_path='data/stop_words_review.csv')
     #query_list_syn = query_expansion.expansion_magic(query, 8, stopwords_path='data/stop_words_synopsis.csv')
     
     # lsi
-    review_pred = lsi.predict_movies(query_list_review, corpus_path='data/lsiReviewCorpus.txt', dic_path='data/lsiReviewDic.dict')
-    syn_pred = lsi.predict_movies(query_list_syn, corpus_path='data/lsiSynCorpus.txt', dic_path='data/lsiSynDic.dict')
+    review_pred = w_rl * lsi.predict_movies(query_list_review, corpus_path='data/lsiReviewCorpus.txt', dic_path='data/lsiReviewDic.dict')
+    syn_pred = w_sl * lsi.predict_movies(query_list_syn, corpus_path='data/lsiSynCorpus.txt', dic_path='data/lsiSynDic.dict')
     
     #doc2vec
-    review_pred += doc2vec.predict_movies(input_list = query_list_review, model_path='data/doc2vecReview.model')
-    syn_pred += doc2vec.predict_movies(input_list = query_list_syn, model_path='data/doc2vecSynopsis.model')
+    review_pred += w_rd * doc2vec.predict_movies(input_list = query_list_review, model_path='data/doc2vecReview.model')
+    syn_pred += w_sd * doc2vec.predict_movies(input_list = query_list_syn, model_path='data/doc2vecSynopsis.model')
 
     # fasttext
-    review_pred += fasttext.predict(model_path='data/fasttext_review', word_list=query_list_review)
-    syn_pred    += fasttext.predict(model_path='data/fasttext_synopsis', word_list=query_list_syn)
+    review_pred += w_rf * fasttext.predict(model_path='data/fasttext_review', word_list=query_list_review)
+    syn_pred    += w_sf * fasttext.predict(model_path='data/fasttext_synopsis', word_list=query_list_syn)
     
     # TFIDF
-    review_pred += TFIDF.TFIDF_pred_review(query = query_list_review, csv_path = '917datafin.csv')
-    syn_pred    += TFIDF.TFIDF_pred_synopsis(query = query_list_syn, csv_path = '917datafin.csv')
+    review_pred += w_rt * TFIDF.TFIDF_pred_review(query = query_list_review, csv_path = 'data/917datafin.csv')
+    syn_pred    += w_st * TFIDF.TFIDF_pred_synopsis(query = query_list_syn, csv_path = 'data/917datafin.csv')
 
     pred_list = w_review * review_pred + w_syn * syn_pred
 
--- main.py
+++ main.py
@@ -10,20 +10,7 @@ import run
 args = sys.argv
 input_text = args[1]
 
-# titles, rates = run.search_for_movies(query=input_text, topn=10, w_review = 1, w_syn = 1, w_r_d=1, w_r_f=1, w_r_l=1, w_r_t=1)
-run.search_for_movies(query=input_text, topn=10, w_review = 1, w_syn = 1, w_r_d=1, w_r_f=1, w_r_l=1, w_r_t=1)
-# input_list = preprocess_TextToList(input_text)
-# input_list = query_expansion.expansion_magic(input_text, 5)  # クエリ拡張機能の実装、引数５は５倍拡張すると言う意味
+titles, rates = run.search_for_movies(query=input_text, topn=10)
 
-# pred_list = lsi.predict_movies(input_list)
-# titles_list = pd.read_csv("data/movies.csv")['title'].values.tolist()
-# sorted_id = np.argsort(pred_list)[::-1]
-# print(f"{titles_list[sorted_id[0]]} : {pred_list[sorted_id[0]]}")
-# print(f"{titles_list[sorted_id[1]]} : {pred_list[sorted_id[1]]}")
-# print(f"{titles_list[sorted_id[2]]} : {pred_list[sorted_id[2]]}")
-# print(f"{titles_list[sorted_id[3]]} : {pred_list[sorted_id[3]]}")
-# print(f"{titles_list[sorted_id[4]]} : {pred_list[sorted_id[4]]}")
-# print(lsi.predict_movies(input_list))
-# print(doc2vec.predict_movies(input_list, topn=2000, model_path='data/doc2vec1005id.model'))
-#print(titles)
-#print(rates)
+print(titles)
+print(rates)
